f7de8d4b1cae0a5a70118a58e86ada86b92395d5
Dang Huy <83294822+danghuy1714@users.noreply.github.com>
2024-06-13 00:57:58 +0700

    win

---
+++ cpu_scheduling/src/run.bat
@@ -0,0 +1,3 @@
+g++ main.cpp Algorithm.cpp FCFS.cpp SJF.cpp RR.cpp Process.cpp Priority.cpp ../include/Algorithm.h ../include/FCFS.h ../include/SJF.h ../include/RR.h ../include/Process.h ../include/Priority.h -o run
+run
+del run.exe 
