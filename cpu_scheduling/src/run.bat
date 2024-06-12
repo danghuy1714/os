2496fe90aa4cc9f13f85e9466f2dd2af79500007
Dang Huy <83294822+danghuy1714@users.noreply.github.com>
2024-06-13 01:32:25 +0700

    Delete cpu_scheduling/src/run.bat

--- cpu_scheduling/src/run.bat
+++
@@ -1,3 +0,0 @@
-g++ main.cpp Algorithm.cpp FCFS.cpp SJF.cpp RR.cpp Process.cpp Priority.cpp ../include/Algorithm.h ../include/FCFS.h ../include/SJF.h ../include/RR.h ../include/Process.h ../include/Priority.h -o run
-run
-del run.exe 
